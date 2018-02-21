8e892123f318338ea74b186d2254f676ef2f948c
KPtch <36377768+KPtch@users.noreply.github.com>
2018-02-21 22:22:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,25 +8,35 @@ server.listen(process.env.port || process.env.PORT || 3978, function(){
     console.log('%s listening to %s', server.name, server.url);
 });
 
-// Setup Bot
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+// // Setup Bot
+// var connector = new builder.ChatConnector({
+//     appId: process.env.MICROSOFT_APP_ID,
+//     appPassword: process.env.MICROSOFT_APP_PASSWORD
+// });
+// var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector, function(session){
+    session.send("Hello, Welcome");
+    var token = tokens[session.userId];
+    //How do I get the user Id here?
+    if(token == undefined){
+        session.beginDialog('login');
+    } else {
+        session.userData.token = token;
+        session.send("How can I help you?");
+        session.send(token);
+    }
+
 });
-var bot = new builder.UniversalBot(connector);
 
 
+// var msg = server.post('api/messages', connector.listen());
 
-
-var msg = server.post('api/messages', connector.listen());
-
-bot.dialog('/', function (session) {
-    session.send("สวัสดีจ้า");
-    session.send(msg);
+// bot.dialog('/', function (session) {
+//     session.send("สวัสดีจ้า");
+//     session.send(msg);
     
-    popup.postMessage("The user is 'bob' and the password is 'secret'",
-                  "https://www.facebook.com/");
+
+// });
 
 
-});
 
