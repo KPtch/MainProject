f0c5c0d21e2d120a7e09c68f30d59913873d3877
KPtch <36377768+KPtch@users.noreply.github.com>
2018-02-22 00:15:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ bot.dialog('/', function (session) {
     session.send(session.message.text);
     session.send('-----------------');
 //     session.send(session.threadID);
-    session.send(session.userid);
+//     session.send(session.userid);
     session.send(session.user_id);
     session.send(session.user.id);
     session.send(session.id);
