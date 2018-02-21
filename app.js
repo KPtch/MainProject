5cff23571efff0d9fc4ae07b14415b8c8c13b4c2
KPtch <36377768+KPtch@users.noreply.github.com>
2018-02-21 22:22:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ server.listen(process.env.port || process.env.PORT || 3978, function(){
     console.log('%s listening to %s', server.name, server.url);
 });
 
-// // Setup Bot
-// var connector = new builder.ChatConnector({
-//     appId: process.env.MICROSOFT_APP_ID,
-//     appPassword: process.env.MICROSOFT_APP_PASSWORD
-// });
+// Setup Bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 // var bot = new builder.UniversalBot(connector);
 var bot = new builder.UniversalBot(connector, function(session){
     session.send("Hello, Welcome");
