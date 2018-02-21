0da5e4b2942c75de6c1efa572d9a617e87bd6c88
KPtch <36377768+KPtch@users.noreply.github.com>
2018-02-22 00:58:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 var restify = require('restify');
 var builder = require('botbuilder');
-// var handleMessage = require('./handleMessage.js');
+var data = require('./respond.json');
 
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function(){
@@ -20,20 +20,18 @@ var timeout = undefined;
 
 var msg = server.post('api/messages', connector.listen());
 
-var data  = {
-    "A": "https://goo.gl/MWUi5Y",
-    "B" : "https://goo.gl/eikbr8",
-    "C": "https://goo.gl/yFcAZQ"};
+// var data  = {
+//     "A": "https://goo.gl/MWUi5Y",
+//     "B" : "https://goo.gl/eikbr8",
+//     "C": "https://goo.gl/yFcAZQ"};
 
 var res;
 bot.dialog('/', function (session) {
     session.send("สวัสดีจ้า");
     session.send(session.message.text);
+    session.send(data.A);
     var req = session.message.text;
     session.send(req);
-//     res = handleMessage(req);
-//     session.send(req);
-//      session.send(res);
 });
 
 
