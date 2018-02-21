cfab4f65cfcf67ff556a32a09e874e32945d0bfe
KPtch <36377768+KPtch@users.noreply.github.com>
2018-02-21 23:34:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ bot.dialog('/', function (session) {
     
     
     if(session.message.text == "คำร้องทั่วไป"){
-        session.send("สวัสดีจ้า2");
+        session.send("นี้จร้า ^^");
         session.send(myObj.A);
      }
     
