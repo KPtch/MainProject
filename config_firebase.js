4dfeb6409d10a760a76f438b8f361ad593c0d99d
KPtch <pop_29033@hotmail>
2018-05-16 00:47:39 +0700

    submit
    up all new file at 16/05/2018

--- config_firebase.js
+++
@@ -1,5 +0,0 @@
-var firebase = require('firebase');
-firebase.initializeApp({
-    databaseURL: 'https://ksbot-test.firebaseio.com/',
-    serviceAccount: 'ksbot-test-dec.json', //this is file that I downloaded from Firebase Console
-});
